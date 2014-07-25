73192f5064c27cc85faa168a97140f9ae0cc499f
taiyeb zahir <taiyebzahir13@gmail.com>
2014-07-25 15:09:16 +0200

    Update example_perfect_pattern.py

--- example_perfect_pattern.py
+++ example_perfect_pattern.py
@@ -6,34 +6,31 @@ import numpy
 from utils import generate_pop, HexGrid, draw_hex_grid
 from stops_ import Stops2
 
-secretion = numpy.array([4, 5, 6])
-reception = numpy.array([2, 3, 1])
-receptors = numpy.array([1, -1, -1])
-bound=numpy.array([1,1,1,1,1,1,1])
+secretion = numpy.array([0,5])
+reception = numpy.array([3,4])
+receptors = numpy.array([-1,-1])
+bound=numpy.array([1,1,1,1,1,1])
 
-base1=numpy.array([0,1,0,0,0,0,0])
-base2=numpy.array([0,0,0,0,0,0,0])
+base1=numpy.array([0,0,1,0,0,0])
+base2=numpy.array([0,0,0,0,0,0])
 
 
-trans_mat = numpy.array([[0,-1,0,0,0,10,0], #notch
-                         [0,0,0,0,1,0,0], #Delta
-                         [0.0005,0,0,0,0,0,0], #delta receptor
-                         [-10,0,0,0,0,0,1], #notch receptor
-                         [0,0,0,0,0,0,0], #ligand_delta
-                         [0,0,0,0,0,0,0], #ligand_notch
-			 [0,0,0,0,0,0,0], #dummy_for_activating_differentiation
+trans_mat = numpy.array([[0,0,0,0,0,0], #Delta_ligand
+                         [20,0,2,0,0,0], #dummy
+			 [0,0.0005,0,0,1,0], #Delta
+                         [0,-1,-2,0,0,1], #notch receptor
+                         [0,0,1,0,0,0],#basal
+                         [0,0,0,0,0,0] #sequential activation
                          ])
 
 init_pop = generate_pop([(50, base1), (2450, base2)])
 grid = HexGrid(50, 50, 1)
 
 def color_fun(row):
-    if row[0]==1:
+    if row[2]==1:
         return 1
     elif row[3]==1:
         return 0.75
-    elif row[1]==1:
-        return 0.3
     else:
         return 0.
 
@@ -41,7 +38,7 @@ def color_fun(row):
 
 def run():
     x = Stops2(trans_mat, init_pop, grid.adj_mat, bound, secretion, reception, receptors, secr_amount=6, leak=0, max_con=6, max_dist=1.5, opencl=False)
-    for i in range(15000):
+    for i in range(10000):
         x.step()
         if i%100 == 0:
             print i
